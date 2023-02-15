f4705d109f8ba3397e851df36d074a088cc90b79
Igor Juan <110120641+1gorjuan@users.noreply.github.com>
2023-02-15 00:00:34 -0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,27 @@
+async function sendScript(scriptText) {
+  const lines = scriptText.split(/[\n\t]+/).map(line => line.trim()).filter(line => line);
+  const main = document.querySelector("#main");
+  const textarea = main.querySelector(`div[contenteditable="true"]`);
+
+  if (!textarea) throw new Error("Não há uma conversa aberta");
+
+  for (const line of lines) {
+    console.log(line);
+
+    textarea.focus();
+    document.execCommand('insertText', false, line);
+    textarea.dispatchEvent(new Event('change', { bubbles: true }));
+
+    setTimeout(() => {
+      (main.querySelector(`[data-testid="send"]`) || main.querySelector(`[data-icon="send"]`)).click();
+    }, 100);
+
+    if (lines.indexOf(line) !== lines.length - 1) await new Promise(resolve => setTimeout(resolve, 250));
+  }
+
+  return lines.length;
+}
+
+sendScript(`
+// o texto será colocado nessa linha, para adcionar mais mensagens só pular a linha
+`).then(e => console.log(`Código finalizado, ${e} mensagens enviadas`)).catch(console.error);
